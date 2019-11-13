6f100e7de581b921c6f960f4b5810f7382ddcae3
Michael Dwan <m@dwan.io>
2019-11-13 09:59:26 -0600

    Update fly.rb

--- Formula/fly.rb
+++ Formula/fly.rb
@@ -1,9 +1,9 @@
 class Fly < Formula
   desc "fly cli"
   homepage "https://fly.io"
-  url "https://get.fly.io/tarballs/stable/fly-v0.54.4/fly-v0.54.4-darwin-x64.tar.gz"
-  sha256 "56c666fd38c8013cb4ecc35c031eb77a850c040b60e7b6fc654748c7fb3e63ca"
-  version "0.54.4"
+  url "https://get.fly.io/tarballs/stable/fly-v0.54.5/fly-v0.54.5-darwin-x64.tar.gz"
+  sha256 "2fb22d7668144d45e0e81997871d786837584abbb9cde5158ef3367caf0f4db0"
+  version "0.54.5"
 
   def install
     inreplace "bin/fly", /^CLIENT_HOME=/, "export FLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
