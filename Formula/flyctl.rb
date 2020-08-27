1f5c56eaf6b1239467e734dd0dea35c7c69fd08f
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-27 11:51:48 -0600

    Brew formula update for flyctl version v0.0.140

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -2,16 +2,16 @@
 class Flyctl < Formula
   desc ""
   homepage "https://fly.io"
-  version "0.0.139"
+  version "0.0.140"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.139/flyctl_0.0.139_macOS_x86_64.tar.gz"
-    sha256 "fbe7f5e849b034f4b8fc66ca1ff15293e00881ca4e279c8c0085e3667e38025b"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.140/flyctl_0.0.140_macOS_x86_64.tar.gz"
+    sha256 "c22050d2261738d2f5056220ffb77463dcfb6a657bae58a0ac7034f67d099c64"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/superfly/flyctl/releases/download/v0.0.139/flyctl_0.0.139_Linux_x86_64.tar.gz"
-      sha256 "405cc7c5fe72fd6db6c30d53973da2ed6c0f6bcc8645fe79271675b6b5e7018e"
+      url "https://github.com/superfly/flyctl/releases/download/v0.0.140/flyctl_0.0.140_Linux_x86_64.tar.gz"
+      sha256 "0dc836749030ae5c1469e4bb0a005f7ada0e080cc6cdcb63fae2c9f0c8efc35d"
     end
   end
 
