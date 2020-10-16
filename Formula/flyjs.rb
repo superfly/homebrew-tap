411381d8ca64d347f9ef6cdf79c19492c9e798eb
Michael Dwan <m@dwan.io>
2020-10-16 14:47:43 -0500

    rename fly => flyjs

--- Formula/flyjs.rb
+++ Formula/flyjs.rb
@@ -1,17 +1,17 @@
-class Fly < Formula
-  desc "fly cli"
+class Flyjs < Formula
+  desc "flyjs cli"
   homepage "https://fly.io"
   url "https://get.fly.io/tarballs/stable/fly-v0.56.0/fly-v0.56.0-darwin-x64.tar.gz"
   sha256 "c07ee5f33c0611e935b832871e9aeb230200367cdb4be2643969e3f7e0deccec"
   version "0.56.0"
 
   def install
-    inreplace "bin/fly", /^CLIENT_HOME=/, "export FLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/flyjs", /^CLIENT_HOME=/, "export FLY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/fly"
+    bin.install_symlink libexec/"bin/flyjs"
   end
 
   test do
-    system bin/"fly", "version"
+    system bin/"flyjs", "version"
   end
 end
