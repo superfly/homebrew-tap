9f85dc42f9f727c3ce0e86b62aa8bf3f20115f0c
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-01 11:59:48 -0700

    Brew formula update for flyctl version v0.0.167

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -5,20 +5,20 @@
 class Flyctl < Formula
   desc ""
   homepage "https://fly.io"
-  version "0.0.166"
+  version "0.0.167"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.166/flyctl_0.0.166_macOS_x86_64.tar.gz"
-    sha256 "f696d389d108a1c53692e8bfe216f7b0aea2e1e21ef28f8c1034c9ecc62cb3fd"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.167/flyctl_0.0.167_macOS_x86_64.tar.gz"
+    sha256 "8407ebe9b8c76948fc2b65f13c1410a185e70a75b99cc57a03c53f70ebc53646"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.166/flyctl_0.0.166_Linux_x86_64.tar.gz"
-    sha256 "3031251405869b0bddf57a907ccafa3d5a219c51770e9ac881a06ed4d3299f69"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.167/flyctl_0.0.167_Linux_x86_64.tar.gz"
+    sha256 "36eaeeaa0107b0ece6b1f4dfa8d0a9d914967bbdcd20f59e5beab9f0c1f22019"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.166/flyctl_0.0.166_Linux_arm64.tar.gz"
-    sha256 "b84035f8d9d8f8d1d8f7d202db94af719dab7d832ca0264cd0b17b2ece264145"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.167/flyctl_0.0.167_Linux_arm64.tar.gz"
+    sha256 "467a2343145ed50ec24bd812ecf8acea44b7b79ee27247cfede96fa6a55026dd"
   end
 
   def install
