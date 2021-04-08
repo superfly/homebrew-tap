46ded6a47fdccb42512ec3f2f2d13e07816c5b25
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-08 17:32:52 -0600

    Brew formula update for flyctl version v0.0.206

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -5,24 +5,24 @@
 class Flyctl < Formula
   desc ""
   homepage "https://fly.io"
-  version "0.0.205"
+  version "0.0.206"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.205/flyctl_0.0.205_macOS_x86_64.tar.gz"
-    sha256 "05ccee582d7abefe83bbb76317745e75549ec8d8774b8921e0f0d0d888148fa5"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.206/flyctl_0.0.206_macOS_x86_64.tar.gz"
+    sha256 "e635d55a8f73c37859428cca82c41f5d13673bc89fa365f9ae5eb0837393a7c3"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.205/flyctl_0.0.205_macOS_arm64.tar.gz"
-    sha256 "87d947802f920365599f42292a1ed8eddaa43982956322eef49718a9ff642f64"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.206/flyctl_0.0.206_macOS_arm64.tar.gz"
+    sha256 "92ab72af0ab6eb997090d3053d10b374fa35851989b6d2fe4951bd3facbf170e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.205/flyctl_0.0.205_Linux_x86_64.tar.gz"
-    sha256 "5d60b034bba6eb0f7dcb86a33fc74f6a1128b5c47702fbd5f199c4b9f86ebbc3"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.206/flyctl_0.0.206_Linux_x86_64.tar.gz"
+    sha256 "225d784f91d70d55ee68c06199377a6d96724202b01c242248c90d16b8b6a7cc"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.205/flyctl_0.0.205_Linux_arm64.tar.gz"
-    sha256 "7a06140ff91d7fa9e9d9ee4f963c34ab7894ab89039fd203db77f03993c0a859"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.206/flyctl_0.0.206_Linux_arm64.tar.gz"
+    sha256 "98f50bbc002ddb249c04999c10273891b7360b8d53b3f80f15fb9aacb073f566"
   end
 
   def install
