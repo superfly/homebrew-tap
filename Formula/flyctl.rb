3471f37a9d1ababb6526df70b3195203a0048e29
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-10 18:04:58 -0600

    Brew formula update for flyctl version v0.0.217

--- Formula/flyctl.rb
+++ Formula/flyctl.rb
@@ -5,24 +5,24 @@
 class Flyctl < Formula
   desc ""
   homepage "https://fly.io"
-  version "0.0.217-pre-3"
+  version "0.0.217"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.217-pre-3/flyctl_0.0.217-pre-3_macOS_x86_64.tar.gz"
-    sha256 "05910de1b6349b0d0663fe567928f99259b5c03eb0dd9fd679e304fa8d13065e"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.217/flyctl_0.0.217_macOS_x86_64.tar.gz"
+    sha256 "b4493bf82fcc85a1e5a740ab696c15fff86d408a2755fab4dc5e2ee7af05cd39"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.217-pre-3/flyctl_0.0.217-pre-3_macOS_arm64.tar.gz"
-    sha256 "239fdebe5d39580c632454c2911588dadc6e3580fab8205bcc0998f84e949323"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.217/flyctl_0.0.217_macOS_arm64.tar.gz"
+    sha256 "e835c6eea6c76a83ecb5b74b6e2469152cb6aaf24a134a8f37c4615eefcb161f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.217-pre-3/flyctl_0.0.217-pre-3_Linux_x86_64.tar.gz"
-    sha256 "8c05206a0df845005c5e8005ab573141dbfcd34f6b18b76b290d68bafc3174ff"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.217/flyctl_0.0.217_Linux_x86_64.tar.gz"
+    sha256 "5b4a41284853b2a17ebe0527d5a047757a721d622d0ee85a7fb87e20cde4d586"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/superfly/flyctl/releases/download/v0.0.217-pre-3/flyctl_0.0.217-pre-3_Linux_arm64.tar.gz"
-    sha256 "3e6a92d8fe4bb0d02e7f8b3d1ccbe40f65f4d28118a3c6b1214eff9f275e3bc7"
+    url "https://github.com/superfly/flyctl/releases/download/v0.0.217/flyctl_0.0.217_Linux_arm64.tar.gz"
+    sha256 "e8a6abe68ff84bfe1fd33cedc9e571bdd04c3eed3de5d6efbad895bdc5e7b11f"
   end
 
   def install
